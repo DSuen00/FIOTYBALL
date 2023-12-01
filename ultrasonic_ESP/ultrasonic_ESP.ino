8e5927d95e3aea287a4c384c276476f4b2e35135
Ray Wong <sleepyyreii@gmail.com>
2023-11-30 23:45:45 -0800

    finished? ultrasonic ESP code

--- ultrasonic_ESP/ultrasonic_ESP.ino
+++ ultrasonic_ESP/ultrasonic_ESP.ino
@@ -4,12 +4,12 @@
 # define trigPinOpp 26
 # define echoPinOpp 25
 # define trigPinPla 12
-# define echoPinOpp 27
+# define echoPinPla 27
 
 WiFiClient client;
 
 const char* ssid = "Berkeley-IoT";
-const char* password = "yNxq)I&2";
+const char* password = "40#u4552";
 
 //Your Domain name with URL path or IP address with path
 String serverName = "http://10.43.67.246";
@@ -22,16 +22,17 @@ long durationOpp; int distanceOpp;
 long durationPla;int distancePla;
 
 int score = 0;
-int countOpp = 0; int countPla = 0;
+int count = 0; int countOpp = 0; int countPla = 0;
 volatile bool new_game = false;
 
 const int distance_reset_min_opp = 5000;
 const int distance_reset_max_opp = 11000;
 
-const int distance_reset_min = 5000;
-const int distance_reset_max = 11000;
+const int distance_reset_min_pla = 5000;
+const int distance_reset_max_pla = 11000;
 
 unsigned long previousTime = 0;
+unsigned long lastTime = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -45,10 +46,10 @@ void loop() {
   // put your main code here, to run repeatedly:
   unsigned long currentTime = micros();
   if (currentTime - previousTime < 120) {
-    digitalWrite(trigPin, HIGH);
+    digitalWrite(trigPinPla, HIGH); digitalWrite(trigPinOpp, HIGH);
 //    Serial.print(1);
   } else if (currentTime - previousTime < 160) {
-    digitalWrite(trigPin, LOW);
+    digitalWrite(trigPinPla, LOW); digitalWrite(trigPinOpp, LOW);
     scoretracker();
   } else {
     previousTime = currentTime;}
@@ -66,13 +67,13 @@ void setup_wifi(){
 
 void scoretracker() {
   durationPla = pulseIn(echoPinPla, HIGH);
-  distancePla = duration * SOUND_SPEED/2;
+  distancePla = durationPla * SOUND_SPEED/2;
   durationOpp = pulseIn(echoPinOpp, HIGH);
-  distanceOpp = duration * SOUND_SPEED/2;
+  distanceOpp = durationOpp * SOUND_SPEED/2;
   
   if (!new_game) {
     if (distancePla > distance_reset_min_pla && distancePla < distance_reset_max_pla
-        ) {
+        && distanceOpp > distance_reset_min_opp && distanceOpp < distance_reset_max_opp) {
       count += 1;
       if (count == 1000) { count = 0; new_game = true; }
     } else {
@@ -81,23 +82,27 @@ void scoretracker() {
   }
 
   if (new_game) {
-    if (distance <= distance_reset_min || distance >= distance_reset_max) {
-      count += 1;
-      if (count == 100) 
-      {count = 0; score += 1; new_game = false;
+    if (distancePla <= distance_reset_min_pla || distancePla >= distance_reset_max_pla) {
+      countPla += 1;
+      if (countPla == 100) 
+      {countPla = 0; new_game = false;
       bool sent = false;
       while (!sent){
-      sent = sentGoal(true);} }
-    } else {
-      count = 0;
-      }
+      sent = sentGoal(true);}}
+    } else {countPla = 0;}
+    if (distanceOpp <= distance_reset_min_opp || distanceOpp >= distance_reset_max_opp) {
+      countOpp += 1;
+      if (countOpp == 100) 
+      {countOpp = 0; new_game = false;
+      bool sent = false;
+      while (!sent){
+      sent = sentGoal(false);}}
+    } else {countOpp = 0;}
   }
 
   // Prints the distance in the Serial Monitor
-  Serial.print("Distance : ");
-  Serial.print(distance);
-  Serial.print("   Score : ");
-  Serial.println(score);
+  Serial.print("Distance Pla: "); Serial.print(distancePla);
+  Serial.print("Distance Opp: "); Serial.print(distanceOpp);
 }
 
 bool sentGoal(bool player){
