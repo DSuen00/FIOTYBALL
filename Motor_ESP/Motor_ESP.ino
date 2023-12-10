eeb3b0896b2c9deb902ee0aaa6f4b25764459360
DSuen00 <90784748+DSuen00@users.noreply.github.com>
2023-12-09 22:19:15 -0800

    Update Motor_ESP.ino
    Commented

--- Motor_ESP/Motor_ESP.ino
+++ Motor_ESP/Motor_ESP.ino
@@ -3,6 +3,7 @@
 
 //PINOUTS
 
+// G: Goalkeeper shaft
 #define LIMIT_UP_G 19
 #define LIMIT_DOWN_G 20
 
@@ -11,6 +12,7 @@
 #define MOTOR_LEFT_G 4
 #define MOTOR_RIGHT_G 5
 
+// P: Other shaft
 #define LIMIT_UP_P 32
 #define LIMIT_DOWN_P 14
 
@@ -22,17 +24,17 @@
 #define wifiLED 22
 
 // STATES
-#define IDLE 0
-#define UP 1
-#define DOWN 2
-#define NO_UP 3
-#define NO_DOWN 4
+#define IDLE 0 // when there is no input from the player
+#define UP 1 // when player presses up
+#define DOWN 2 // when player presses down
+#define NO_UP 3 // when up limit switch is activated; disables up movement
+#define NO_DOWN 4 // when down limit switch is activated; disables down movement
 
 int lin_state_G = 0;
 int rot_state_G = 0;
 int lin_state_P = 0;
 int rot_state_P = 0;
-String data = "1111";
+String data = "1111"; // string away telling motors what to do
 WiFiClient client;
 
 const char* ssid = "sleepy";
@@ -41,12 +43,10 @@ const char* ssid = "sleepy";
 const char* password = "pleasew0rk";
 
 //Your Domain name with URL path or IP address with path
-// String serverName = "http://10.44.65.112";
 String serverName = "http://192.168.229.42";
 const int serverPort = 8000;
 String serverPath = "/motor";
 unsigned long lastTime = 0;
-// Set timer to 5 seconds (5000)
 unsigned long timerDelay = 10;
 
 void setup() {
@@ -64,10 +64,10 @@ void loop() {
  data = getServerRequest();
  Serial.print(data[0]);Serial.print(data[1]);
  Serial.println(" ");
- lin_state_G = linear_motor_G(lin_state_G);
- rot_state_G = rot_motor_G(rot_state_G);
- lin_state_P = linear_motor_P(lin_state_P);
- rot_state_P = rot_motor_P(rot_state_P);
+ lin_state_G = linear_motor_G(lin_state_G); // tells motors for linear movement what to do
+ rot_state_G = rot_motor_G(rot_state_G); // tells motors for rotational movement what to do
+ lin_state_P = linear_motor_P(lin_state_P); // tells motors for linear movement what to do
+ rot_state_P = rot_motor_P(rot_state_P); // tells motors for rotational movement what to do
  Serial.println(digitalRead(LIMIT_DOWN_G));
  Serial.println(digitalRead(LIMIT_UP_G));
  delay(1);
@@ -125,19 +125,19 @@ int linear_motor_G(int state){
 
       // Serial.println("IDLE");
       
-      if (data[1] == '0') {
+      if (data[1] == '0') {  // when player inputs up
         digitalWrite(MOTOR_UP_G, HIGH);
         state = UP;}
       
-      if (data[1] == '2') {
+      if (data[1] == '2') {  // when player inputs down
         digitalWrite(MOTOR_DOWN_G, HIGH);
         state = DOWN;
       }
       
-      if (digitalRead(LIMIT_UP_G) == HIGH) {
+      if (digitalRead(LIMIT_UP_G) == HIGH) { // when up limit switch is activated
         state = NO_UP;}
 
-      if (digitalRead(LIMIT_DOWN_G) == HIGH) {
+      if (digitalRead(LIMIT_DOWN_G) == HIGH) { // when down limit switch is activated
         state = NO_DOWN;}
 
       break;
@@ -146,11 +146,11 @@ int linear_motor_G(int state){
 
       Serial.println("UP");
       
-      if (data[1] == '1') {
+      if (data[1] == '1') { // when player lets go of up button
         digitalWrite(MOTOR_UP_G, LOW);
         state = IDLE;}
       
-      if (digitalRead(LIMIT_UP_G) == HIGH) {
+      if (digitalRead(LIMIT_UP_G) == HIGH) { // when up limit switch is hit; cuts off up motor
         digitalWrite(MOTOR_UP_G, LOW);
         state = NO_UP;}
       break;
@@ -159,11 +159,11 @@ int linear_motor_G(int state){
 
       Serial.println("DOWN");
       
-      if (data[1] == '1') {
+      if (data[1] == '1') { // when player lets go of up button
         digitalWrite(MOTOR_DOWN_G, LOW);
         state = IDLE;}
       
-      if (digitalRead(LIMIT_DOWN_G) == HIGH) {
+      if (digitalRead(LIMIT_DOWN_G) == HIGH) { // when up limit switch is hit; cuts off down motor
         digitalWrite(MOTOR_DOWN_G, LOW);
         state = NO_DOWN;}
       break;
@@ -172,10 +172,10 @@ int linear_motor_G(int state){
 
       Serial.println("NO_UP");
       
-      if (digitalRead(LIMIT_UP_G) == LOW) {
+      if (digitalRead(LIMIT_UP_G) == LOW) { // when limit switch is deactivated
         state = IDLE;}
 
-      if (data[1] == '2') {
+      if (data[1] == '2') {   // when player inputs down
         digitalWrite(MOTOR_DOWN_G, HIGH);
         state = DOWN;}
       break;
@@ -184,10 +184,10 @@ int linear_motor_G(int state){
 
       Serial.println("NO_DOWN");
       
-      if (digitalRead(LIMIT_DOWN_G) == LOW) {
+      if (digitalRead(LIMIT_DOWN_G) == LOW) { // when limit switch is deactivated
         state = IDLE;}
 
-      if (data[1] == '0') {
+      if (data[1] == '0') {  // when player inputs up
         digitalWrite(MOTOR_UP_G, HIGH);
         state = UP;}
       break;}
@@ -200,19 +200,19 @@ int linear_motor_P(int state){
 
       // Serial.println("IDLE");
       
-      if (data[3] == '0') {
+      if (data[3] == '0') {  // when player inputs up
         digitalWrite(MOTOR_UP_P, HIGH);
         state = UP;}
       
-      if (data[3] == '2') {
+      if (data[3] == '2') {  // when player inputs down
         digitalWrite(MOTOR_DOWN_P, HIGH);
         state = DOWN;
       }
       
-      if (digitalRead(LIMIT_UP_P) == HIGH) {
+      if (digitalRead(LIMIT_UP_P) == HIGH) { // when up limit switch is hit
         state = NO_UP;}
 
-      if (digitalRead(LIMIT_DOWN_P) == HIGH) {
+      if (digitalRead(LIMIT_DOWN_P) == HIGH) {  // when down limit switch is hit
         state = NO_DOWN;}
 
       break;
@@ -221,11 +221,11 @@ int linear_motor_P(int state){
 
       Serial.println("UP");
       
-      if (data[3] == '1') {
+      if (data[3] == '1') {  // when player lets go of up
         digitalWrite(MOTOR_UP_P, LOW);
         state = IDLE;}
       
-      if (digitalRead(LIMIT_UP_P) == HIGH) {
+      if (digitalRead(LIMIT_UP_P) == HIGH) {  // when limit switch of up is hit; cuts off up motor
         digitalWrite(MOTOR_UP_P, LOW);
         state = NO_UP;}
       break;
@@ -234,11 +234,11 @@ int linear_motor_P(int state){
 
       Serial.println("DOWN");
       
-      if (data[3] == '1') {
+      if (data[3] == '1') {  // when player lets go of down
         digitalWrite(MOTOR_DOWN_P, LOW);
         state = IDLE;}
       
-      if (digitalRead(LIMIT_DOWN_P) == HIGH) {
+      if (digitalRead(LIMIT_DOWN_P) == HIGH) {  // when down limit swich is hit
         digitalWrite(MOTOR_DOWN_P, LOW);
         state = NO_DOWN;}
       break;
@@ -247,10 +247,10 @@ int linear_motor_P(int state){
 
       Serial.println("NO_UP");
       
-      if (digitalRead(LIMIT_UP_P) == LOW) {
+      if (digitalRead(LIMIT_UP_P) == LOW) { // when up limit switch is let go of
         state = IDLE;}
 
-      if (data[3] == '2') {
+      if (data[3] == '2') { // when player inputs down
         digitalWrite(MOTOR_DOWN_P, HIGH);
         state = DOWN;}
       break;
@@ -259,10 +259,10 @@ int linear_motor_P(int state){
 
       Serial.println("NO_DOWN");
       
-      if (digitalRead(LIMIT_DOWN_P) == LOW) {
+      if (digitalRead(LIMIT_DOWN_P) == LOW) {  // when down limit switch is let go of
         state = IDLE;}
 
-      if (data[3] == '0') {
+      if (data[3] == '0') {  // when player inputs up
         digitalWrite(MOTOR_UP_P, HIGH);
         state = UP;}
       break;}
@@ -276,11 +276,11 @@ int rot_motor_G(int state){
 
       // Serial.println("IDLE");
       
-      if (data[motor] == '2') {
+      if (data[motor] == '2') { // when player inputs left
         digitalWrite(MOTOR_LEFT_G, HIGH);
         state = UP;}
       
-      if (data[motor] == '0') {
+      if (data[motor] == '0') {  // when player inputs right
         digitalWrite(MOTOR_RIGHT_G, HIGH);
         state = DOWN;}
 
@@ -290,7 +290,7 @@ int rot_motor_G(int state){
 
       Serial.println("UP");
       
-      if (data[motor] == '1') {
+      if (data[motor] == '1') {  // when player lets go of left
         digitalWrite(MOTOR_LEFT_G, LOW);
         state = IDLE;}
       break;
@@ -299,7 +299,7 @@ int rot_motor_G(int state){
 
       Serial.println("DOWN");
       
-      if (data[motor] == '1') {
+      if (data[motor] == '1') {  // when player lets go of right
         digitalWrite(MOTOR_RIGHT_G, LOW);
         state = IDLE;}
       break;}
@@ -313,11 +313,11 @@ int rot_motor_P(int state){
 
       // Serial.println("IDLE");
       
-      if (data[motor] == '2') {
+      if (data[motor] == '2') {  // when player inputs left
         digitalWrite(MOTOR_LEFT_P, HIGH);
         state = UP;}
       
-      if (data[motor] == '0') {
+      if (data[motor] == '0') {  // when player inputs right
         digitalWrite(MOTOR_RIGHT_P, HIGH);
         state = DOWN;}
 
@@ -327,7 +327,7 @@ int rot_motor_P(int state){
 
       Serial.println("UP");
       
-      if (data[motor] == '1') {
+      if (data[motor] == '1') {  // when player lets go of left
         digitalWrite(MOTOR_LEFT_P, LOW);
         state = IDLE;}
       break;
@@ -336,7 +336,7 @@ int rot_motor_P(int state){
 
       Serial.println("DOWN");
       
-      if (data[motor] == '1') {
+      if (data[motor] == '1') {  // when player lets go of right
         digitalWrite(MOTOR_RIGHT_P, LOW);
         state = IDLE;}
       break;}
